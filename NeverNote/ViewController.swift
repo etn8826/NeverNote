840f16a95bc39fdb92e801806c2e687fa1d991b2
Einstein Nguyen <etn8826@gmail.com>
2020-04-27 16:48:33 -0400

    Initial Commit

---
+++ NeverNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeverNote
+//
+//  Created by Einstein Nguyen on 4/27/20.
+//  Copyright © 2020 Einstein Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
